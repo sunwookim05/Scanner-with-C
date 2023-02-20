acade76e04cc84b6c6b56c9c784c7f08b1f1e8d4
kimsunwoo <sunwookim05@gmail.com>
2023-02-20 23:48:44 +0900

    Master

--- src/Systemout.c
+++ src/Systemout.c
@@ -4,4 +4,4 @@
 void print(const String format, ...) {va_list ap;char buf[4096];va_start(ap, format);vsprintf(buf, format, ap);va_end(ap);fprintf(stdout, "%s", buf);}
 void println(const String format, ...) {va_list ap;char buf[4096];va_start(ap, format);vsprintf(buf, format, ap);va_end(ap);fprintf(stdout, "%s\n", buf);}
 
-Sys System = {print, println};
+SYSTEM System = {print, println};
